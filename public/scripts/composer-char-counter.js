67486b002db201ddb08f0bdc5f8920ab298ce684
Kazuko Baynton <kazukobaynton@gmail.com>
2022-03-18 23:38:36 -0700

    create composer-char-counter.js

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $("#tweet-text").on("input", function(){
+    // tweet element values are texts, we want the length
+    const maxLength = 140;
+    let currentLength = ($(this).val().length);
+    let remaining = maxLength - currentLength;
+    // use jQuery to traverse the DOM tree from that node/element
+    let counter = ($(this).next().children(".counter"));
+    counter.html(remaining);
+
+    if (remaining < 0) {
+      // counter.css('color', 'red')
+      counter.addClass("myClass");
+    } else {
+      // counter.css('color', '#545149')
+      counter.removeClass("myClass");
+    }
+  })
+
+
+
+
+
+  $("button").on('click', function() {
+    alert("button clicked!")
+  });
+});
